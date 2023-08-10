52fa90ab1cc6929e31ad45568844e820898ebd76
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-10 09:17:25 +0530

    Find twisted prime
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 03_TwistedPrime.cpp
+++ 03_TwistedPrime.cpp
@@ -6,7 +6,27 @@ using namespace std ;
 class Solution{
 public: 
     bool isTwistedPrime(int n) {
-        
+        int rev = 0, temp = n;
+        while(temp) {
+            rev = rev * 10 + (temp % 10);
+            temp /= 10;
+        }
+        return isPrime(rev) && isPrime(n);
+    }
+
+    bool isPrime(int n)
+    {
+        if (n < 2) return false;
+
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i*i <= n; i = i + 6)
+        {
+            if (n % i == 0 || n % (i+2) == 0) return false;
+        }
+
+        return true;
     }
 };
 
