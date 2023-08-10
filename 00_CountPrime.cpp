53cd0cb7f810ce634eeabe4557a19f7e2b607006
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-10 09:15:34 +0530

    TLE cls
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 00_CountPrime.cpp
+++ 00_CountPrime.cpp
@@ -4,6 +4,7 @@
 using namespace std ;
 
 class Solution {
+    // TLE
 public:
     int countPrimes(int n) {
         if (n < 2) return 0;
@@ -26,7 +27,7 @@ public:
 
         for (int i = 5; i*i <= n; i = i + 6)
         {
-            if (n % i == 0) return false;
+            if (n % i == 0 || n % (i+2) == 0) return false;
         }
 
         return true;
---
+++ 03_TwistedPrime.cpp
@@ -0,0 +1,16 @@
+// Twisted prime: A number is called a twisted prime if it is prime and it's reverse is also prime.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution{
+public: 
+    bool isTwistedPrime(int n) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
