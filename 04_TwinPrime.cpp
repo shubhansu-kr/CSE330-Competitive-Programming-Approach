00089d06737ec5abd1a1c5a179b46bdc6f8b0b99
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-10 09:22:33 +0530

    FInd twin primes
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 04_TwinPrime.cpp
@@ -0,0 +1,40 @@
+// A pair of prime number is said to be twin prime if difference between the primes is 2.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public: 
+    int countTwinPrime (int n) {
+        int count = 0;
+        for (int i = 2; i < n; ++i)
+        {
+            if (isPrime(i), isPrime(i+2)) {
+                ++count;
+                cout << i << " " << i+2 << " \n";
+            }
+        }
+        return count;
+    }
+
+    bool isPrime(int n)
+    {
+        if (n < 2) return false;
+
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i*i <= n; i = i + 6)
+        {
+            if (n % i == 0 || n % (i+2) == 0) return false;
+        }
+
+        return true;
+    }
+};
+
+int main () {
+    Solution obj;
+    cout << "\n\n\n\n count : " <<  obj.countTwinPrime(1000);
+    return 0;
+}
