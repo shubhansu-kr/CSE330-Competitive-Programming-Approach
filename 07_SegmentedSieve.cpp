96f10ddc2e7364506c08943d2bc3a0dab5c11ef1
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-14 09:56:48 +0530

    Incomplete
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 07_SegmentedSieve.cpp
@@ -0,0 +1,23 @@
+// Segmented Sieve Algorithm: 
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+// Divide the range [0, n-1] in different segments.
+// Size of each segment is root n.
+// Compute primes in all segments one by one.
+
+// TC: O(n*loglog(n)) 
+// SC: O(root(n) + l)                           : l -> number of primes 
+
+class Solution {
+public: 
+    vector<int> segmentedSieve(int n) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
