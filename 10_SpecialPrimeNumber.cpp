3bb083d8bf96f7809e7f4bbe897bb8feb5c134e5
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-17 09:43:47 +0530

    Check For special prime
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 10_SpecialPrimeNumber.cpp
+++ 10_SpecialPrimeNumber.cpp
@@ -8,7 +8,31 @@ using namespace std ;
 
 class Solution {
 public: 
-    
+    bool isSpecialPrime(int n) {
+        int k = n;
+        
+        while(k) {
+            if (!isPrime(k)) return false;
+            k /= 10;
+        }
+
+        return true;
+    }
+
+    bool isPrime(int n)
+    {
+        if (n < 2) return false;
+
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i*i <= n; i = i + 6)
+        {
+            if (n % i == 0 || n % (i+2) == 0) return false;
+        }
+        
+        return true;
+    }
 };
 
 int main () {
