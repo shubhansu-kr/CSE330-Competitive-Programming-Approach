476a92f03338dc1b30968de255b3cfb9d0515301
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-17 09:41:57 +0530

    Find least prime factor
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 09_LeastPrimeFactor.cpp
+++ 09_LeastPrimeFactor.cpp
@@ -1,9 +1,38 @@
-// Find the least prime factor.
+// Find the least prime factor of all the number from 2 to n.
 
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution {
+public: 
+    vector<int> leastPrimeFactor(int n) {
+        vector<int> ans(n+1, -1);
 
+        // Prime factor of all the prime number is 2
+        for (int i = 2; i <= n; ++i)
+        {
+            ans[i] = returnPrime(i);
+        }
+
+        return ans;
+    }
+
+    int returnPrime(int n)
+    {
+        if (n < 2) return -1;
+
+        if (n % 2 == 0) return 2;
+        if (n % 3 == 0) return 3;
+
+        for (int i = 5; i*i <= n; i = i + 6)
+        {
+            if (n % i == 0 ) return i;
+            if (n % (i+2) == 0) return i+2;
+        }
+
+        return n;
+    }
+};
 
 int main () {
     
---
+++ 10_SpecialPrimeNumber.cpp
@@ -0,0 +1,17 @@
+// A number n is called special prime if n is prime, n/10 is also prime, n/100 is also prime
+// untill n/10*d is also prime where d is no. of digits -1;
+
+// eg. 473 -> check for 473, 47 and 4;
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public: 
+    
+};
+
+int main () {
+    
+    return 0;
+}
