79dd421c6b68f978a49b3c8885e639e05e71c860
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-17 12:39:39 +0530

    TEST
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ Test.cpp
@@ -0,0 +1,26 @@
+// Test
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minOperation(vector<int> &A, int B) {
+        if (B > A.size()) return 0;
+
+        unordered_set<int> st;
+        for(auto &it: A) {st.insert(it);}
+
+        int D = st.size();`
+
+        if (D < B) return 0;
+        return abs(B-D);
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
