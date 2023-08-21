85f5f0d5088be9c3f47c5267d871899e0582b6e1
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-21 09:12:07 +0530

    TEST
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Test.cpp
+++ Test.cpp
@@ -3,6 +3,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1 {
+public: 
+    int countPairs(vector<int> &A) {
+        int n = A.size();
+        int count = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (2 * (A[i] | A[j]) + (A[i] ^ A[j]) >= A[i] + A[j]) {
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
 class Solution
 {
 public:
