86c3e587d73477715acebb07aa263b1e9bd0791b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-22 09:48:09 +0530

    Find the Nth fortunate number
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 16_NthFortunateNumber.cpp
@@ -0,0 +1,18 @@
+// Find the nth fortunate number: you are given a number n. Find the first n primes
+// take their product and find the min. number to be added to the product to
+// make it prime.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+int nthFortunate(int n) {
+    // According to euclid's theorem... 
+    // for all the values of n we have to add just 1 to make the product 
+    // prime
+    return 1;
+}
+
+int main () {
+    
+    return 0;
+}
