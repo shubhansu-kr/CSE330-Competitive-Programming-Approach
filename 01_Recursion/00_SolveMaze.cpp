703ee6257f9394eace44ff81ccd61e17d7874f36
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-31 09:48:18 +0530

    Solution1: Recursion
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_Recursion/00_SolveMaze.cpp
+++ 01_Recursion/00_SolveMaze.cpp
@@ -3,6 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1{
+    // Recursion
+public: 
+    // 'S' -> Source cell
+    // 'E' -> Destination cell
+    // '*' -> obstacle.
+
+    bool dfs(vector<string> &board, vector<vector<int>> &visited, int i, int j) {
+        if (i < 0 || j < 0 || i >= visited.size() || j >= visited[0].size()) return false;
+        if (visited[i][j]) return false;
+
+        if (board[i][j] == 'E') return true;
+        visited[i][j] = 1;
+
+        if (dfs(board, visited, i-1, j)) return true;
+        if (dfs(board, visited, i, j+1)) return true;
+        if (dfs(board, visited, i+1, j)) return true;
+        if (dfs(board, visited, i, j-1)) return true;
+
+        return false;
+    }
+
+    bool isPathAvailable(vector<string> board, int row, int col){
+        int m = board.size(), n = board[0].length();
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+
+        return dfs(board, visited, row, col);
+    }
+};
+
 class Solution{
 public: 
     // 'S' -> Source cell
