ef0d3ef71d66a6777ade980d411c011e600f1115
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-05 09:58:31 +0530

    Incomplete
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_Recursion/07_NextHappyNumber.cpp
@@ -0,0 +1,19 @@
+// Given a number n, find the min number greater than or equal to n which is
+// a happy number
+
+// https://practice.geeksforgeeks.org/problems/next-happy-number4538/1
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int nextHappyNumber(int n) {
+
+    }
+};
+
+int main () {
+    
+    return 0;
+}
