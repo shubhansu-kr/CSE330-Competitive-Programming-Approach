de1450ce5661541293de47bc0c996a6aba114752
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-07 09:49:28 +0530

    BruteForce: Recursion
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_Recursion/08_SisterCoin.cpp
+++ 01_Recursion/08_SisterCoin.cpp
@@ -4,6 +4,25 @@
 using namespace std ;
 
 class Solution{
+    // BruteForce: Recursion
+    // TLE
+public: 
+    bool solve(int arr[], int &n, int m, int sum, int i){
+        if (i < 0) return false;
+        if (sum <= m) return sum == m;
+
+        return solve(arr, n, m+arr[i], sum-arr[i], i-1) || solve(arr, n, m, sum, i-1);
+    }
+
+    int sisterCoin(int arr[], int n, int m){
+        int sum = 0;
+        for (int i = 0; i < n; ++i){sum += arr[i];}
+        
+        return solve(arr, n, m, sum, n-1);
+    }
+};
+
+class Solution{
     // TLE: Time limit exceeded.
 public: 
     bool solve(int arr[], int &n, int m, int sum, int i){
