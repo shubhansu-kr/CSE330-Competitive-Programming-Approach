2579802ff7ab76158e540afbd9f4666f27d19f92
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-26 09:35:54 +0530

    Solution2: Tabulation
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/00_FibonacciSeries.cpp
+++ 02_DynammicProgramming/00_FibonacciSeries.cpp
@@ -1,7 +1,26 @@
 // Fibonacci Series: f(n) = f(n-1) + f(n-2): f(0) = 0, f(1) = 1
+// Overlapping Subproblem
 
 #include <bits/stdc++.h>
 using namespace std ;
+
+class Solution2 {
+    // Tabulation
+    // DP can be applied only when there is an overlapping subproblem.
+public: 
+    
+    int fibo(int n) {
+        if (n < 2) return n;
+        vector<int> dp(n+1, -1);
+
+        dp[0] = 0, dp[1] = 1;
+        for(int i = 2; i < n+1; ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
 
 class Solution1 {
     // Recursion: Memoization
