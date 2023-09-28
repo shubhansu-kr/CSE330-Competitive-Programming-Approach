9bebb9a82c78fae742aa64ffa7cfee774b88d1bf
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-28 09:24:58 +0530

    Solution2: Memoization
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/01_KnapSack.cpp
+++ 02_DynammicProgramming/01_KnapSack.cpp
@@ -4,6 +4,34 @@
 using namespace std ;
 
 class Solution1{
+    // Recursion: Memoization
+public: 
+    int solve(vector<vector<int>> &dp, vector<int> &values, vector<int> &weight, int bag, int i) {
+        if (i < 0 || bag <= 0) return 0;
+
+        if (dp[i][bag] != -1) return dp[i][bag];
+
+        // pick
+        int pick = INT_MIN;
+        if (bag >= weight[i]) {
+            pick = values[i] + solve(dp, values, weight, bag-weight[i], i-1);
+        }
+        // noPick
+        int noPick = solve(dp, values, weight, bag, i-1);
+
+        return dp[i][bag] = max(pick, noPick);
+    }
+
+    int knapSack(vector<int> &values, vector<int> &weight, int bag) {
+        int n = values.size();
+
+        vector<vector<int>> dp(n, vector<int>(bag+1, -1));
+
+        return solve(dp, values, weight, bag, n-1);
+    }
+};
+
+class Solution1{
     // Recursion: Optimised
 public: 
     int solve(vector<int> &values, vector<int> &weight, int bag, int i) {
@@ -16,12 +44,13 @@ public:
         }
         // noPick
         int noPick = solve(values, weight, bag, i-1);
+
+        return max(pick, noPick);
     }
 
     int knapSack(vector<int> &values, vector<int> &weight, int bag) {
         int n = values.size();
 
-        int ans = INT_MIN, steal = 0;
         return solve(values, weight, bag, n-1);
     }
 };
