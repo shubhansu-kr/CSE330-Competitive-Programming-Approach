2401b9838f0f7197d6dfd8288e84dd5dbb554a49
Shubhansu kumar <shubhansu2021@gmail.com>
2023-09-28 09:21:14 +0530

    Solution1: Recursion Optimised
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/01_KnapSack.cpp
+++ 02_DynammicProgramming/01_KnapSack.cpp
@@ -3,6 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1{
+    // Recursion: Optimised
+public: 
+    int solve(vector<int> &values, vector<int> &weight, int bag, int i) {
+        if (i < 0 || bag <= 0) return 0;
+
+        // pick
+        int pick = INT_MIN;
+        if (bag >= weight[i]) {
+            pick = values[i] + solve(values, weight, bag-weight[i], i-1);
+        }
+        // noPick
+        int noPick = solve(values, weight, bag, i-1);
+    }
+
+    int knapSack(vector<int> &values, vector<int> &weight, int bag) {
+        int n = values.size();
+
+        int ans = INT_MIN, steal = 0;
+        return solve(values, weight, bag, n-1);
+    }
+};
+
 class Solution{
     // BruteForce: Recursion
 public: 
