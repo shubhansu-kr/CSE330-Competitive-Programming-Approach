a13c208159c35cd12617ac1fdb7a5d279c15ca9a
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-03 09:23:46 +0530

    Solution1: Memozation
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/04_SubsetSum.cpp
+++ 02_DynammicProgramming/04_SubsetSum.cpp
@@ -9,6 +9,29 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
+class Solution1{  
+    // Recursion: Memoization
+public:
+    bool solve(vector<int> &arr, vector<vector<int>> &dp, int sum, int i) {
+        if (sum == 0) return true;
+        if (i < 0 || sum < 0) return false;
+        
+        if (dp[i][sum] != -1) return dp[i][sum];
+
+        if (solve(arr, dp, sum-arr[i], i-1)) return dp[i][sum] = true;
+        if (solve(arr, dp, sum, i-1)) return dp[i][sum] = true;
+
+        return dp[i][sum] = false;
+    }
+
+    bool isSubsetSum(vector<int>arr, int sum){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int> (sum+1, -1));
+
+        return solve(arr, dp, sum, arr.size()-1);
+    }
+};
+
 class Solution{  
     // Recursion:  
 public:
