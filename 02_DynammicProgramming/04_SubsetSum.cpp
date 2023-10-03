f0bf7dd0ea5b52f28231f0505aa72cb7ddb92249
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-03 09:20:39 +0530

    check if sum exists in subset
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 02_DynammicProgramming/04_SubsetSum.cpp
@@ -0,0 +1,52 @@
+// https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{  
+    // Recursion:  
+public:
+    bool solve(vector<int> &arr, int sum, int i) {
+        if (sum == 0) return true;
+        if (i < 0 || sum < 0) return false;
+        
+        if (solve(arr, sum-arr[i], i-1)) return true;
+        if (solve(arr, sum, i-1)) return true;
+
+        return false;
+    }
+
+    bool isSubsetSum(vector<int>arr, int sum){
+        return solve(arr, sum, arr.size()-1);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
