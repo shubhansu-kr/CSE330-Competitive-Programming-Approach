5d273868662433be5e30aec771105a689eb763de
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-09 09:38:33 +0530

    Unbounded knapsack: Repetition of items allowed.
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/03_UnboundedKnapSack.cpp
+++ 02_DynammicProgramming/03_UnboundedKnapSack.cpp
@@ -1,11 +1,57 @@
-// 
+// https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+//{ Driver Code Starts
+// Initial Template for C++
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
+// } Driver Code Ends
+// User function Template for C++
 
+class Solution{
+    // BruteForce: Recursion
+public:
+    void solve(int val[], int wt[], int &ans, int &sum, int W, int i) {
+        if (i < 0 || W <= 0) {
+            ans = max(ans, sum);
+            return;
+        }
 
-int main () {
-    
+        if (W >= wt[i]) {
+            sum += val[i];
+            solve(val, wt, ans, sum, W-wt[i], i);
+            sum -= val[i];
+        }
+
+        solve(val, wt, ans, sum, W, i-1);
+    }
+
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        int ans = INT_MIN, sum = 0;
+        solve(val, wt, ans, sum, W, N-1);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, W;
+        cin>>N>>W;
+        int val[N], wt[N];
+        for(int i = 0;i < N;i++)
+            cin>>val[i];
+        for(int i = 0;i < N;i++)
+            cin>>wt[i];
+        
+        Solution ob;
+        cout<<ob.knapSack(N, W, val, wt)<<endl;
+    }
     return 0;
 }
+// } Driver Code Ends
