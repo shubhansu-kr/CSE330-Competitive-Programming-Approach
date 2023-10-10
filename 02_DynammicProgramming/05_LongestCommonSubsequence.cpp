9e951c9a1b57e3ccdc3016c85724114691143b5b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-10 09:47:07 +0530

    Solution3: Space optimisation
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/05_LongestCommonSubsequence.cpp
+++ 02_DynammicProgramming/05_LongestCommonSubsequence.cpp
@@ -3,6 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution3 {
+    // Tabulation: Space Optimisation
+public:
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length(), n = text2.length();
+        if (m == 0 || n == 0) return 0;
+
+        vector<int> temp(n+1);
+        
+        for(int i = 1; i <= m; ++i) {
+            vector<int> curr(n+1);
+            for(int j = 1; j <= n; ++j) {
+                if (text1[i-1] == text2[j-1]) curr[j] = 1 + temp[j-1];
+                else curr[j] = max(temp[j], curr[j-1]);
+            }
+            temp = curr;
+        }
+
+        return temp[n];
+    }
+};
+
 class Solution2 {
     // Tabulation
 public:
