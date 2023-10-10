de163e6d80732e90079c665e1c985e53d0735240
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-10 09:42:37 +0530

    Solution2: Tabulation
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 02_DynammicProgramming/05_LongestCommonSubsequence.cpp
+++ 02_DynammicProgramming/05_LongestCommonSubsequence.cpp
@@ -3,7 +3,28 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
-class Solution {
+class Solution2 {
+    // Tabulation
+public:
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length(), n = text2.length();
+        if (m == 0 || n == 0) return 0;
+
+        vector<vector<int>> dp(m+1, vector<int> (n+1));
+        
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                if (text1[i-1] == text2[j-1+]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+class Solution1 {
     // Recursion: Memoization
 public:
     int solve(vector<vector<int>> &dp, string &text1, string &text2, int i, int j){
